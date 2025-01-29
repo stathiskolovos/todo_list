aa4f896b05c365e97e56c2aa65e93dae6497544b
Stathis Kolovos <stathiskolovos@MacBook.local>
2025-01-29 16:48:56 +0200

    setting initial state

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  
 
+  const [todos, setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState('');
+  const [listInputs, setListInputs] = useState({});
+  
   return (
     <>
       <div className="todo-container">
